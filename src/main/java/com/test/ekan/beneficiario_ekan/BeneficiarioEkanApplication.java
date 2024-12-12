82c180a5d695f99b728861831cfbe9daf48fd972
Victor <victormota_03@hotmail.com>
2024-12-12 19:59:25 -0300

    first commit

---
+++ src/main/java/com/test/ekan/beneficiario_ekan/BeneficiarioEkanApplication.java
@@ -0,0 +1,13 @@
+package com.test.ekan.beneficiario_ekan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiarioEkanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiarioEkanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/ekan/beneficiario_ekan/BeneficiarioEkanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ekan.beneficiario_ekan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficiarioEkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
