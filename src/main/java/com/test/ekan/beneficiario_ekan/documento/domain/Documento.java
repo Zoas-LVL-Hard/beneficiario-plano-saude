57d1fc73fceae28d12aafc1bf4f725afb8e1efd4
Victor <victormota_03@hotmail.com>
2024-12-22 17:37:42 -0300

    Configura domain-documento

---
+++ src/main/java/com/test/ekan/beneficiario_ekan/documento/domain/Documento.java
@@ -0,0 +1,33 @@
+package com.test.ekan.beneficiario_ekan.documento.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "documento")
+public class Documento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private long id;
+    @NotBlank
+    private String tipoDocumento;
+    @NotBlank
+    private String descricao;
+    private LocalDateTime dataDeInclusao;
+    private LocalDateTime dataDeAtualizacao;
+}
